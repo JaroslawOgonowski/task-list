1827853731b4e505203dfcddeb0b2c1be3c969f7
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2022-10-17 14:16:57 +0200

    js- remove coments and console log

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
 {
-    const tasks = [ //task będzie tablicą złożoną z 2 obiektów
-        {// pierwszy obiekt
-            content: "nagrać lekcję",
-            done: false,
-        },
-        {// drugi obiekt
-            content: "zjeść pierogi",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({//obiekt
+        tasks.push({
             content: newTaskContent,
         });
 
@@ -53,45 +44,38 @@
         });
     };
 
-    const render = () => {//funkcja render ma wyrenderować nowe zadanie żeby było widoczne 
-        //i tak: najpierw ustalamy htmlString jako pusty łańcuch nazków, potem dla każdego task w tasks
-        //dopiszemy coś w htmlString dzięki += można dopisywać do wartości
-        //przy czym każdy htmlString będzie elementem listy z treścią
-        //z obiektu task i jego wartością content
+    const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li class="list__itemContainer">
-                <button class="js-done list__button">${task.done ? "&#10004":""}</button>
+                <button class="js-done list__button">${task.done ? "&#10004" : ""}</button>
                 <span class="list__item${task.done ? " list__item--done" : ""}">${task.content}</span>    
                 <button class="js-remove list__button list__button--red">&#128465</button>
                  
             </li>
-        `;/*tłumaczenie przekreślenia: sprawdza czy wartość done od task jest true czy false jeśli true to skreśla jeśli false to nic nie robi */
-    }
+        `;
+        }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
         bindEvents();
-        
+
     };
 
 
 
     const onFormSubmit = (event) => {
-        event.preventDefault();//zwykły preventDefault
+        event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();//ustanawia jako newTaskContent wartość formularza z htmla i trimuje czyli usuwa białe znaki z prawej i lewej strony
-        console.log(newTaskContent);
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
         if (newTaskContent === "") {
-            return; //jeśli okno jest puste to nic nie robimy
+            return;
         }
         addNewTask(newTaskContent);
         focusOnSubmit();
-        //a jeśli nie jest pusty to chcemy dodać nowe zadanie (czyli nowy obiekt do tablicy):
-        //znów wywołujemy funkcję render i to jest w funkcji newTasContent
     };
 
 
@@ -100,7 +84,7 @@
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", onFormSubmit);//podczas wysłania formularza odpala się funkcja onFormSubmit
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
