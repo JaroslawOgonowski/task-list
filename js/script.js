8fcb10d505bab76a0221e478090cfbb189f9d2f0
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2022-11-02 17:35:13 +0100

    add function to new buttons, respo upgrade

--- js/script.js
+++ js/script.js
@@ -24,17 +24,32 @@
             { ...tasks[taskIndex], done: !tasks[taskIndex].done, },
             ...tasks.slice(taskIndex + 1),
         ];
-
         render();
-
     };
+
+    const markAllTaskDone = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }
+        ))
+        render();
+    };
+
+    const hideDoneTasks = () => {
+        hideDoneTask = !hideDoneTask
+        render();
+    };
+
+
     const focusOnSubmit = () => {
         const newTask = document.querySelector(".js-newTask")
         newTask.value = "";
         newTask.focus();
         render();
     }
-    const bindEvents = () => {
+
+    const removeButtonEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -42,8 +57,9 @@
                 removeTask(index);
             });
         });
+    };
 
-
+    const toggleDoneButtonsEvents = () => {
         const toggleDoneButtons = document.querySelectorAll(".js-done");
 
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
@@ -54,62 +70,70 @@
     };
 
 
+    const bindEvents = () => {
+        removeButtonEvents();
+        toggleDoneButtonsEvents();
+    };
+
+
     const renderButtons = (htmlString) => {
         let subtitleString = "";
         if (htmlString != "") {
             subtitleString +=
                 `<h2 class=" main__subtitle--lower js-subtitle">Lista zadań</h2>
-                 <button class="main__button">Ukryj zakończone</button><button class="main__button">Ukończ wszystkie</button>` }
-
+                 <button class="main__button js-hideDoneTasks">${hideDoneTask ? "Pokaż" : "Ukryj"} zakończone</button>
+                 <button class="main__button js-allTaskDone"
+                 ${tasks.every(({ done }) => done) ? "disabled" : ""}>Ukończ wszystkie</button>`
+        }
         else if (htmlString === "") {
             subtitleString +=
                 `<h2 class=" main__subtitle--lower js-subtitle">Lista zadań</h2>`
         };
-
         document.querySelector(".js-subtitle").innerHTML = subtitleString;
     };
 
     const renderTask = () => {
         let htmlString = "";
-
         for (const task of tasks) {
             htmlString += `
-            <li class="list__itemContainer">
+            <li class="list__itemContainer ${task.done && hideDoneTask ? "list__itemContainer--hidden" : ""}">
                 <button class="js-done list__button">${task.done ? "&#10004" : ""}</button>
                 <span class="list__item${task.done ? " list__item--done" : ""}">${task.content}</span>    
-                <button class="js-remove list__button list__button--red">&#128465</button>
-                 
+                <button class="js-remove list__button list__button--red">&#128465</button>               
             </li>
         `;
         }
-
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
-
-
         renderButtons(htmlString);
     };
 
-
-
-    const bindButtonEvents = () => {/**if */ };
+    const bindButtonEvents = () => {
+        const allTaskDoneButton = document.querySelector(".js-allTaskDone");
+        if (allTaskDoneButton) {
+            allTaskDoneButton.addEventListener("click", () => {
+                markAllTaskDone();
+            })
+        }
+        const hideDoneTasksButton = document.querySelector(".js-hideDoneTasks")
+        if (hideDoneTasksButton) {
+            hideDoneTasksButton.addEventListener("click", () => {
+                hideDoneTasks();
+            })
+        }
+    };
 
     const render = () => {
         renderTask();
 
-
         bindEvents();
         bindButtonEvents();
-
     };
 
 
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
         if (newTaskContent === "") {
             return;
         }
@@ -120,9 +144,7 @@
 
     const init = () => {
         render();
-
         const form = document.querySelector(".js-form");
-
         form.addEventListener("submit", onFormSubmit);
     };
 
